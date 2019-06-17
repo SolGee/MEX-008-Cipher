e8ae7dd5f1eee7d248e135d956b11f4fa265b533
SolGee <solsv00@hotmail.com>
2019-06-17 12:56:59 -0500

    Se agrego función de cifrar, descifrar y reset

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,23 @@
 window.cipher = {
-  encode: () => {
-
+  encode: (offset, string) => {
+    let textCoded = '';
+    let stringMay = string.toUpperCase();
+    for(let i = 0; i < stringMay.length; i++){
+      let stringInAsccii = stringMay.charCodeAt(i);
+      let asciiWithOffset = (stringInAsccii - 65 + offset ) % 26 + 65;
+      let aOffsetToText = String.fromCharCode(asciiWithOffset);
+      textCoded += aOffsetToText;
+    }
+    return textCoded;
   },
-  decode: () => {
-    
+  decode: (offset, string) => {
+    let textDecoded = '';
+    let stringMay = stringMay.toUpperCase();
+    for(let i = 0; i < stringMay.length; i++){
+      let stringInAsccii = stringMay.charCodeAt(i);
+      let asciiWithoutOffset = (stringInAsccii - 65 - offset + 52) % 26 + 65;
+      let fromAsciiToText = String.fromCharCode(asciiWithoutOffset);
+      textDecoded += fromAsciiToText;
+    }return textDecoded;
   }
 };
--- src/index.js
+++ src/index.js
@@ -56,6 +56,66 @@ const closeHelp = () => {
 close.addEventListener('click', closeHelp);
 
 
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// OFFSET (SAVE)
+
+const codeButton = document.getElementById('cifrar'); // Boton para cifrado
+let offset = document.getElementById('offset'); // Offset 
+let msjToCodeDecode = document.getElementById('code-decode'); // Donde se escribira el mensaje SIN cifrar o descifrar
+
+
+// IMPRIMIR MENSAJE CODIFICADO
+
+const newMessageCode = () => { //Arrow function para imprimir un nuevo mensaje (cifrado)
+  const blankSpaceCode = document.getElementById('frase-secreta'); //Donde se mostrara el mensaje YA cifrado
+  let offsetCode = parseInt(offset.value); // parseInt convierte los strings en number. Convierte el valor ingresado en el offset a un valor numérico.
+  let msjCoded = cipher.encode(offsetCode, msjToCodeDecode.value); // Toma la función cipher.encode de cipher.js y asigna los parametros del offset y el valor del mensaje a codificar
+  blankSpaceCode.innerHTML = msjCoded; //imprime en el textarea el valor del mensaje codificado
+}
+
+
+codeButton.addEventListener('click', newMessageCode) //Ejecuta la función newMessageCode
+
+
+
+
+//CIPHER DECODE
+
+// OFFSET (SAVE)
+
+const decodeButton = document.getElementById('descifrar'); // Botón para descifrar
+//No es necesario volver a llamar el offset ni el area donde se pondra el mensaje SIN cifrar o descifrar 
+
+// IMPRIMIR MENSAJE DECODIFICADO
+
+const newMessageDecode = () => { // //Arrow function para imprimir un nuevo mensaje (descifrado)
+  const blankSpaceDecode = document.getElementById('frase-secreta'); //Donde se mostrara el mensaje YA descifrado 
+  let offsetDecode = parseInt(offset.value);
+  let msjDecoded = cipher.decode(offsetDecode, msjToCodeDecode.value); // Toma la función cipher.decode de cipher.js y asigna los parametros del offset y el valor del mensaje a decodificar
+  blankSpaceDecode.innerHTML = msjDecoded; // imprime en el textarea el valor del mensaje DECODIFICADO
+
+}
+
+decodeButton.addEventListener('click', newMessageDecode); //Ejecuta la función newMessageDecode
+
+
+
+// BOTON LIMPIAR
+
+const cleanButton = document.getElementById('limpiar');
+
+const cleanTextarea = () => {
+  document.getElementById('formulario').reset();
+
+}
+  
+
+
+cleanButton.addEventListener('click', cleanTextarea);
+
 
 
 
